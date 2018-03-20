1ee39c177bc137273d4c8c1ee5e8a002c42a62fa
Z F <215566435@qq.com>
2018-03-21 09:29:33 +1300

    feather sider hide button / fully responsive support

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 
 import { HashRouter as Router, Route } from 'react-router-dom'
-import { Layout } from 'antd'
+import { Layout, Icon } from 'antd'
 import { connect } from 'react-redux'
 
 import SiderRenderer from './views/sider'
@@ -11,8 +11,49 @@ import PageRenderer from './views/page'
 const { Header, Content, Footer, Sider } = Layout
 
 class App extends React.Component {
+    state = {
+        siderCollapsed: false
+    }
+
     componentDidMount() {
         window['$trumpDoc'] = this
+
+        window.addEventListener('resize', this.resize)
+        this.resize()
+    }
+
+    toggle = () => {
+        this.setState(
+            {
+                siderCollapsed: !this.state.siderCollapsed
+            },
+            () => {
+                this.props.dispatch({
+                    type: 'fetchMenu',
+                    payload: !this.state.siderCollapsed
+                })
+            }
+        )
+    }
+
+    resize = () => {
+        if (window.innerWidth < 700) {
+            this.setState({
+                siderCollapsed: true
+            })
+        } else {
+            this.setState(
+                {
+                    siderCollapsed: false
+                },
+                () => {
+                    this.props.dispatch({
+                        type: 'fetchMenu',
+                        payload: false
+                    })
+                }
+            )
+        }
     }
 
     render() {
@@ -26,8 +67,8 @@ class App extends React.Component {
                             position: 'fixed',
                             left: 0
                         }}
-                        breakpoint="lg"
                         collapsedWidth="0"
+                        collapsed={this.state.siderCollapsed}
                         onCollapse={(collapsed, type) => {
                             this.props.dispatch({
                                 type: 'fetchMenu',
@@ -38,8 +79,23 @@ class App extends React.Component {
                         <div className="logo" />
                         <SiderRenderer />
                     </Sider>
-                    <Layout className="trump-layout">
-                        <Header style={{ background: '#fff', padding: 0 }} />
+                    <Layout
+                        className="trump-layout"
+                        style={{
+                            marginLeft: this.state.siderCollapsed ? 0 : 200
+                        }}
+                    >
+                        <Header style={{ background: '#fff', padding: 0 }}>
+                            <Icon
+                                className="trigger"
+                                type={
+                                    this.state.siderCollapsed
+                                        ? 'menu-unfold'
+                                        : 'menu-fold'
+                                }
+                                onClick={this.toggle}
+                            />
+                        </Header>
                         <Content
                             style={{
                                 margin: '24px 16px 0',
