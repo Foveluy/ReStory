1535f70c12acb6a20e23cc70cab75a5fab2b123c
Z F <215566435@qq.com>
2018-03-21 15:46:37 +1300

    draw canvas down

--- src/component/canvas.js
+++ src/component/canvas.js
@@ -13,6 +13,14 @@ class Ball {
         this.interval = 17
         this.distanceX = 0
         this.distanceY = 0
+        const r = Math.round(Math.random() * 10)
+        this.r = r
+
+        //到达最大次数的时候，会一直原地抖动
+        this.has_interval = 0
+
+        this.shake = []
+        this.isNextShake = true
     }
 
     setDes = (x, y) => {
@@ -23,15 +31,46 @@ class Ball {
         this.distanceY = this.desY - this.y
     }
 
+    shaking = () => {
+        if (this.isNextShake) {
+            const shakeFator =
+                Math.random() * 7 * ((Math.random() * 10) % 2 === 0 ? 1 : -1)
+
+            for (let i = 0; i < 68; i++) {
+                this.shake.push(shakeFator / 34)
+            }
+            this.isNextShake = false
+            this.y = this.desY
+        } else {
+            const _delt = this.shake.shift()
+
+            if (this.shake.length < 34) {
+                this.y = this.y - _delt
+            } else {
+                this.y = this.y + _delt
+            }
+
+            if (this.shake.length === 0) this.isNextShake = true
+        }
+    }
+
     next = () => {
-        this.x = this.distanceX / 17 + this.x
-        this.y = this.distanceY / 17 + this.y
+        if (this.has_interval < 18) {
+            this.x = this.distanceX / 17 + this.x
+            this.y = this.distanceY / 17 + this.y
+        } else {
+            if (this.has_interval % 4 === 0) {
+                this.shaking()
+            }
+        }
+
+        this.has_interval = this.has_interval + 1
     }
 
     draw = context => {
         context.beginPath()
-        context.fillStyle = `#${this.x}6${this.y}`
-        context.arc(this.x, this.y, 5, 0, Math.PI * 2)
+        context.fillStyle = `#${this.r}6${this.r}`
+        context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
         context.closePath()
         context.fill()
     }
@@ -39,14 +78,14 @@ class Ball {
 
 export class Canvas extends React.Component {
     componentDidMount() {
-        this.zi = ['s','c','d','x','y']
+        this.zi = ['s', 'c', 'd', 'x', 'y']
         this.drawCanvas()
     }
 
     drawCanvas = () => {
         const context = this.canvas.getContext('2d')
-        const width = 300
-        const height = 300
+        const width = 400
+        const height = 400
 
         context.clearRect(0, 0, width, height)
         context.fillStyle = 'black'
@@ -56,7 +95,6 @@ export class Canvas extends React.Component {
         context.fillText(this.zi.shift(), width / 2, height / 2)
         const imgData = context.getImageData(0, 0, width, height).data
 
-        console.log(this.zi)
         var gap = 13
         var pos = []
         var x = 0,
@@ -83,7 +121,6 @@ export class Canvas extends React.Component {
 
         let store = []
 
-
         pos.forEach(({ x, y }) => {
             const b = new Ball(width / 2, height / 2)
             b.setDes(x, y)
@@ -97,10 +134,7 @@ export class Canvas extends React.Component {
             })
         }
 
-        var index = 0
         const render = () => {
-            index++
-            if (index > 17) return
             context.clearRect(0, 0, width, height)
 
             draw()
@@ -121,8 +155,8 @@ export class Canvas extends React.Component {
                 onClick={this.handleChange}
                 id="plexus"
                 ref={node => (this.canvas = node)}
-                width={300}
-                height={300}
+                width={400}
+                height={400}
                 style={{ zIndex: 10 }}
             />
         )
